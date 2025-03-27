fa7386fea704f169d4d168fd43ea4753826d8789
Likith Reddy Chintala <98326823+Likithreddy18@users.noreply.github.com>
2025-03-27 15:25:01 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Starting forecast_agent..."
 python forecast_agent.py
-
+echo "Removing duplicates..."
+python Duplicates_removal.py
 echo "Starting FastAPI server..."
 uvicorn forecast_api:app --host 0.0.0.0 --port 8000
